56cfd62dcfc555cf95ee93a29ec15ca35f17de79
KenyanKidd <kk18257588078@gmail.com>
2024-10-14 19:12:42 +0800

    checkpoint2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,33 +1,33 @@
 #include "tcp_receiver.hh"
-#include<iostream>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  if(message.RST) reassembler_.reader().set_error();
-  if((!message.SYN && !ISN.has_value()) || (ISN.has_value() && ISN == message.seqno)){
+  if ( message.RST )
+    reassembler_.reader().set_error();
+  if ( ( !message.SYN && !ISN.has_value() ) || ( ISN.has_value() && ISN == message.seqno ) ) {
     return;
   }
-  if(!ISN.has_value()) {
+  if ( !ISN.has_value() ) {
     ISN = message.seqno;
   }
   uint64_t checkpoint = reassembler_.writer().bytes_pushed() + 1;
-  uint64_t abs_seqno = message.seqno.unwrap(*ISN, checkpoint);
+  uint64_t abs_seqno = message.seqno.unwrap( *ISN, checkpoint );
   uint64_t first_index = abs_seqno == 0 ? abs_seqno : abs_seqno - 1;
-  reassembler_.insert(first_index, message.payload, message.FIN);
-
+  reassembler_.insert( first_index, message.payload, message.FIN );
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  TCPReceiverMessage msg{};
-  msg.window_size = min((uint64_t)UINT16_MAX, reassembler_.writer().available_capacity());
+  TCPReceiverMessage msg {};
+  msg.window_size = min( (uint64_t)UINT16_MAX, reassembler_.writer().available_capacity() );
 
   uint64_t abs_seqno = reassembler_.writer().bytes_pushed() + ISN.has_value() + reassembler_.writer().is_closed();
-  if(ISN.has_value()) {
-    msg.ackno = Wrap32::wrap(abs_seqno, *ISN);
+  if ( ISN.has_value() ) {
+    msg.ackno = Wrap32::wrap( abs_seqno, *ISN );
   }
   msg.RST = reassembler_.writer().has_error();
 
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,5 +1,5 @@
 #include "wrapping_integers.hh"
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
@@ -9,13 +9,13 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  Wrap32 ckpt_32 = wrap(checkpoint, zero_point);
+  Wrap32 ckpt_32 = wrap( checkpoint, zero_point );
   uint64_t diff = raw_value_ - ckpt_32.raw_value_;
-  
-  if(diff < (1UL << 31) || checkpoint < (1UL << 32) - diff) {
+
+  if ( diff < ( 1UL << 31 ) || checkpoint < ( 1UL << 32 ) - diff ) {
     return checkpoint + diff;
-  }
-  else return checkpoint - ((1UL << 32) - diff);
+  } else
+    return checkpoint - ( ( 1UL << 32 ) - diff );
   // else{
   //   if((1UL << 31 < diff) && ((1UL << 32) - diff) <= checkpoint) {
   //     return checkpoint + diff;
