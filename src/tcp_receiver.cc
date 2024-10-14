651b4eeee71d868070627d100b8ded5900067001
KenyanKidd <kk18257588078@gmail.com>
2024-10-14 15:03:21 +0800

    checkpoint2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,15 +1,39 @@
 #include "tcp_receiver.hh"
+#include<iostream>
+#include<algorithm>
 
 using namespace std;
 
 void TCPReceiver::receive( TCPSenderMessage message )
 {
-  // Your code here.
-  (void)message;
+  if(message.RST) reassembler_.reader().set_error();
+  if((!message.SYN && !ISN.has_value()) || (ISN.has_value() && ISN == message.seqno)){
+    return;
+  }
+  if(!ISN.has_value()) {
+    ISN = message.seqno;
+  }
+  
+  uint64_t checkpoint = reassembler_.writer().bytes_pushed() + 1;
+  uint64_t abs_seqno = message.seqno.unwrap(*ISN, checkpoint);
+  // cout << abs_seqno << endl;
+  uint64_t first_idnex = abs_seqno == 0 ? abs_seqno : abs_seqno - 1;
+  // cout << first_idnex << message.payload << endl;
+  reassembler_.insert(first_idnex, message.payload, message.FIN);
+
 }
 
 TCPReceiverMessage TCPReceiver::send() const
 {
-  // Your code here.
-  return {};
+  TCPReceiverMessage msg{};
+  msg.window_size = min((uint64_t)UINT16_MAX, reassembler_.writer().available_capacity());
+
+  uint64_t abs_seqno = reassembler_.writer().bytes_pushed() + ISN.has_value() + reassembler_.writer().is_closed();
+  // cout << abs_seqno << endl;
+  if(ISN.has_value()) {
+    msg.ackno = Wrap32::wrap(abs_seqno, *ISN);
+  }
+  msg.RST = reassembler_.writer().has_error();
+
+  return msg;
 }
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,25 @@
 #include "wrapping_integers.hh"
-
+#include<iostream>
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + n;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  Wrap32 ckpt_32 = wrap(checkpoint, zero_point);
+  uint64_t diff = raw_value_ - ckpt_32.raw_value_;
+  
+  if(diff < (1UL << 31) || checkpoint < (1UL << 32) - diff) {
+    return checkpoint + diff;
+  }
+  else return checkpoint - ((1UL << 32) - diff);
+  // else{
+  //   if((1UL << 31 < diff) && ((1UL << 32) - diff) <= checkpoint) {
+  //     return checkpoint + diff;
+  //   }
+  //   else return checkpoint + ((1UL << 32) - (ckpt_32.raw_value_ - raw_value_));
+  // }
 }
